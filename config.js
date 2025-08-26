9d697f2aa779755982b8791d1d12d20134e11939
kingmd786 <shahidkingmd874@gmail.com>
2025-08-27 04:42:09 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "923423706291", // PUT OWNER NUMBER HERE
+  PREFIX: "." // prefix (e.g., ., /, !, *)
+};
